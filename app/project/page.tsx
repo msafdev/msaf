684480c7b27a557aade9d5e4fcc0d8cd650740a9
salman261002 <salmanalfarisi261002@gmail.com>
2024-03-12 23:54:09 +0700

    update intro page

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,16 +1,64 @@
 export default function Introduction() {
   return (
-    <div className="flex h-full grow flex-col">
+    <div className="flex h-full max-w-xl grow flex-col">
       <h1 className="text-2xl font-semibold text-foreground">Introduction</h1>
-      <p className="mt-4 text-sm text-muted-foreground md:text-base">
+      <p className="mt-3 text-sm text-muted-foreground md:text-base">
         Simply built components you can easily <code>copy</code> and{" "}
         <code>paste</code> into your project. Adaptable, reusable, and{" "}
         <span className="font-semibold">obviously</span> open-sourced.
       </p>
-      <blockquote className="mt-4 w-fit border-l-2 border-l-primary bg-accent py-2 pl-4 pr-4 text-xs italic text-accent-foreground md:text-sm">
+      <blockquote className="mt-3 w-fit border-l-2 border-l-primary bg-accent py-2 pl-4 pr-4 text-xs italic text-accent-foreground md:text-sm">
         This is a collection of components I have used in my projects. <br />
         This is <span className="font-semibold">NOT</span> a component library.
       </blockquote>
+      <h1 className="mt-6 text-lg font-semibold text-foreground">Reference</h1>
+      <p className="mt-3 text-sm text-muted-foreground md:text-base">
+        Making this by myself is never possible without the help of others. I
+        took inspiration from many sources and I am grateful for that. Here are
+        the references and resources I used, if you want to check them out:
+      </p>
+      <ul className="mt-3 text-sm md:text-base">
+        <li>
+          <a href="/" className="flex items-center gap-x-2">
+            -
+            <code className="anim font-medium text-muted-foreground hover:text-foreground">
+              radix-ui
+            </code>
+          </a>
+        </li>
+        <li>
+          <a href="/" className="flex items-center gap-x-2">
+            -
+            <code className="anim font-medium text-muted-foreground hover:text-foreground">
+              shadcn-ui
+            </code>
+          </a>
+        </li>
+        <li>
+          <a href="/" className="flex items-center gap-x-2">
+            -
+            <code className="anim font-medium text-muted-foreground hover:text-foreground">
+              flowbite-ui
+            </code>
+          </a>
+        </li>
+        <li>
+          <a href="/" className="flex items-center gap-x-2">
+            -
+            <code className="anim font-medium text-muted-foreground hover:text-foreground">
+              lucide-icons
+            </code>
+          </a>
+        </li>
+        <li>
+          <a href="/" className="flex items-center gap-x-2">
+            -
+            <code className="anim font-medium text-muted-foreground hover:text-foreground">
+              react-icons
+            </code>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
