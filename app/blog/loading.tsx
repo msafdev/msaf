faf7ac5c703e5ba7345dc5fc259be83b85b6c7df
salman261002 <salmanalfarisi261002@gmail.com>
2024-03-12 07:37:00 +0700

    fix skeleton UI

--- app/blog/loading.tsx
+++ app/blog/loading.tsx
@@ -3,7 +3,7 @@ import { BlogSkeleton } from "@/components/ui/skeleton";
 const Loading = () => {
   return (
     <div className="pad-x grid h-fit w-full gap-x-4 gap-y-6 py-4 md:h-full md:grow md:grid-cols-2 md:py-8 xl:grid-cols-3">
-      {Array.from({ length: 4 }).map((_, index) => (
+      {Array.from({ length: 3 }).map((_, index) => (
         <BlogSkeleton key={index} />
       ))}
     </div>
--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -2,11 +2,12 @@ const BlogSkeleton = () => {
   return (
     <div className="flex h-fit w-full flex-col md:flex-row">
       <div className="aspect-[4/3] w-full animate-pulse rounded-md bg-muted md:h-36 md:w-auto" />
-      <div className="flex h-auto w-full flex-col justify-end gap-y-4 md:gap-y-2 md:py-0">
-        <div className="h-6 w-full animate-pulse rounded-md bg-muted md:ml-4 md:w-[80%]" />
+      <div className="flex h-auto w-full flex-col justify-end gap-y-3 py-3 md:gap-y-3 md:py-0">
+        <div className="h-11 w-full animate-pulse rounded-md bg-muted md:ml-3 md:h-6 md:w-[40%]" />
+        <div className="h-11 w-full animate-pulse rounded-md bg-muted md:ml-3 md:h-6 md:w-[75%]" />
         <div className="flex items-center gap-x-3">
-          <div className="h-12 w-12 animate-pulse rounded-full bg-muted md:ml-4" />
-          <div className="h-14 w-full animate-pulse rounded-md bg-muted md:w-[70%]" />
+          <div className="h-8 w-8 animate-pulse rounded-full bg-muted md:ml-3" />
+          <div className="h-12 w-full animate-pulse rounded-md bg-muted md:h-9 md:w-[65%]" />
         </div>
       </div>
     </div>
