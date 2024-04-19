eb6b00ff1c1e73aaa98f38140a41665632f1d748
salman261002 <salmanalfarisi261002@gmail.com>
2024-04-20 03:40:58 +0700

    update component

--- components/doc/button/cyberpunk-button.tsx
+++ components/doc/button/cyberpunk-button.tsx
@@ -10,9 +10,12 @@ const CyberButton = ({
   variant?: "primary" | "secondary" | "destructive" | "outline";
 }) => {
   const buttonVariant = {
-    primary: "bg-transparent text-foreground hover:bg-primary/90 hover:text-primary-foreground",
-    secondary: "bg-transparent text-foreground hover:bg-secondary/90 hover:text-secondary-foreground",
-    destructive: "bg-transparent text-foreground hover:bg-destructive/90 hover:text-destructive-foreground",
+    primary:
+      "bg-transparent text-foreground hover:bg-primary/90 hover:text-primary-foreground",
+    secondary:
+      "bg-transparent text-foreground hover:bg-secondary/90 hover:text-secondary-foreground",
+    destructive:
+      "bg-transparent text-foreground hover:bg-destructive/90 hover:text-destructive-foreground",
     outline: "bg-transparent text-foreground",
   };
 
@@ -33,25 +36,25 @@ const CyberButton = ({
     >
       <div
         className={cn(
-          "anim-slow pointer-events-none absolute left-0 top-0 h-2 w-2 border-l-[2px] border-t-[2px] group-hover:h-full group-hover:rounded-sm group-hover:w-full",
+          "anim-slow pointer-events-none absolute left-0 top-0 h-2 w-2 border-l-[2px] border-t-[2px] group-hover:h-full group-hover:w-full group-hover:rounded-sm",
           borderVariant[variant],
         )}
       />
       <div
         className={cn(
-          "anim-slow pointer-events-none absolute right-0 top-0 h-2 w-2 border-r-[2px] border-t-[2px] group-hover:h-full group-hover:rounded-sm group-hover:w-full",
+          "anim-slow pointer-events-none absolute right-0 top-0 h-2 w-2 border-r-[2px] border-t-[2px] group-hover:h-full group-hover:w-full group-hover:rounded-sm",
           borderVariant[variant],
         )}
       />
       <div
         className={cn(
-          "anim-slow pointer-events-none absolute bottom-0 left-0 h-2 w-2 border-b-[2px] border-l-[2px] group-hover:h-full group-hover:rounded-sm group-hover:w-full",
+          "anim-slow pointer-events-none absolute bottom-0 left-0 h-2 w-2 border-b-[2px] border-l-[2px] group-hover:h-full group-hover:w-full group-hover:rounded-sm",
           borderVariant[variant],
         )}
       />
       <div
         className={cn(
-          "anim-slow pointer-events-none absolute bottom-0 right-0 h-2 w-2 border-b-[2px] border-r-[2px] group-hover:h-full group-hover:rounded-sm group-hover:w-full",
+          "anim-slow pointer-events-none absolute bottom-0 right-0 h-2 w-2 border-b-[2px] border-r-[2px] group-hover:h-full group-hover:w-full group-hover:rounded-sm",
           borderVariant[variant],
         )}
       />
