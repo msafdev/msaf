0b91f8bf395d7222d0f8de1b4f00409849509a6c
salman261002 <salmanalfarisi261002@gmail.com>
2024-04-20 20:42:43 +0700

    update /template

--- app/template/page.tsx
+++ app/template/page.tsx
@@ -1,8 +1,31 @@
-import Image from "next/image";
-
 // Components
-import Counter from "@/components/macro/counter";
 import TemplateCard from "@/components/macro/template-card";
+
+// Images
+import Terma from "@/public/images/templates/terma.png";
+import Countify from "@/public/images/templates/countify.png";
+import { DotsHorizontalIcon } from "@radix-ui/react-icons";
+
+const Templates = [
+  {
+    name: "Terma",
+    url: "https://msaf-terminal.vercel.app",
+    image: Terma,
+    type: "Open Source",
+    stack: ["Next", "Tailwind"],
+    price: "Free",
+    category: "Portfolio",
+  },
+  {
+    name: "Countify",
+    url: "https://countify-wine.vercel.app",
+    image: Countify,
+    type: "Open Source",
+    stack: ["Next", "Tailwind", "Supabase"],
+    price: "Free",
+    category: "Web App",
+  },
+];
 
 const Template = () => {
   const TargetDate = new Date("2024-10-26");
@@ -13,14 +36,32 @@ const Template = () => {
       className="flex h-full w-full grow flex-col gap-y-4 py-4 md:gap-y-8 md:py-8 lg:gap-y-12 lg:py-12"
     >
       <div className="pad-x flex h-full w-full flex-col gap-y-2 md:gap-y-4">
-        <h1 className="text-2xl font-semibold">Template</h1>
-        <Counter targetDate={TargetDate} />
+        <h1 className="text-lg font-semibold xs:text-xl md:text-2xl">
+          Templates
+        </h1>
+        <p className="max-w-lg text-sm text-muted-foreground xs:text-base md:text-lg">
+          Choose from a variety of templates to{" "}
+          <span className="font-medium text-foreground">kickstart</span> your
+          next project. All templates are{" "}
+          <span className="font-medium text-foreground">free</span> and{" "}
+          <span className="font-medium text-foreground">open-sourced</span>. You
+          can also{" "}
+          <span className="font-medium text-foreground">contribute</span> to the
+          project by submitting a pull request. If you have any questions or
+          need help, feel free to{" "}
+          <span className="font-medium text-foreground">reach out</span> to me.
+        </p>
       </div>
       <div className="h-[1px] w-full bg-border" />
-      <div className="pad-x grid h-fit w-full grid-cols-1 gap-x-3 gap-y-2 md:grid-cols-2 md:gap-y-4 lg:grid-cols-3">
-        <TemplateCard />
-        <TemplateCard />
-        <TemplateCard />
+      <div className="pad-x grid h-fit w-full grid-cols-1 gap-4 md:grid-cols-2 md:gap-8 lg:gap-12 xl:grid-cols-3">
+        {Templates.map((template, index) => (
+          <TemplateCard key={index} {...template} />
+        ))}
+        <div className="flex h-full w-full grow items-center justify-center">
+          <p className="max-w-xs text-center text-sm font-semibold italic text-muted-foreground xs:text-base md:text-lg">
+            Currently building a new template. Stay tuned!
+          </p>
+        </div>
       </div>
     </section>
   );
--- components/macro/template-card.tsx
+++ components/macro/template-card.tsx
@@ -1,36 +1,100 @@
 import { SiVercel } from "react-icons/si";
-import CyberButton from "../doc/button/cyberpunk-button";
 
-const TemplateCard = () => {
+// Assets
+import Terma from "@/public/images/templates/terma.png";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import { StarFilledIcon } from "@radix-ui/react-icons";
+
+const TemplateStack = ({ stack }: { stack: string[] }) => {
+  console.log(stack);
   return (
-    <div className="anim group flex w-full cursor-pointer flex-col gap-y-2 rounded border bg-popover py-2 hover:bg-popover/60">
-      <div className="flex w-full items-center gap-x-3 overflow-hidden px-3">
-        <div className="flex aspect-square h-full w-auto items-center justify-center rounded-full border p-3">
-          <SiVercel className="h-full w-full" />
+    <div className="anim absolute -top-full left-0 z-10 flex w-full items-center justify-around border-b bg-popover px-3 group-hover:top-0">
+      {stack.map((item, index) => (
+        <>
+          <code
+            key={index}
+            className="py-3 text-xs leading-none text-foreground"
+          >
+            {item}
+          </code>
+          {index !== stack.length - 1 && (
+            <div className="h-auto w-[1px] self-stretch bg-border" />
+          )}
+        </>
+      ))}
+    </div>
+  );
+};
+
+const TemplateCard = ({
+  name,
+  url,
+  image,
+  type,
+  stack,
+  price,
+  category,
+}: {
+  name: string;
+  url: string;
+  image: StaticImageData;
+  type: string;
+  stack: string[];
+  price: string;
+  category: string;
+}) => {
+  return (
+    <Link
+      href={url}
+      target="_blank"
+      className="anim group relative flex h-fit w-full cursor-pointer flex-col overflow-hidden rounded border bg-popover pt-2.5"
+    >
+      <div className="absolute right-0 top-0 h-fit rounded-bl-lg rounded-tr bg-accent px-2 py-0.5">
+        <code className="text-xs font-medium leading-none text-accent-foreground">
+          {category}
+        </code>
+      </div>
+      <div className="flex w-full items-center gap-x-2 overflow-hidden border-b px-3 pb-3">
+        <div className="flex aspect-square h-full w-auto items-center justify-center rounded-full border p-2">
+          <SiVercel className="h-full w-full text-foreground" />
         </div>
         <div className="flex w-full cursor-pointer flex-col gap-y-1">
           <div className="flex w-full items-center justify-between gap-x-2">
             <h1 className="text-sm font-medium leading-none text-foreground">
-              Name
+              {name}
             </h1>
-            <div className="rounded-full border bg-accent px-1.5 py-0.5">
-              <p className="text-xs leading-none text-accent-foreground">
-                Portfolio
-              </p>
-            </div>
           </div>
           <p className="text-xs leading-none text-muted-foreground">
-            asdjwandnsaoindiojwaioj
+            {url.replace("https://", "")}
           </p>
         </div>
       </div>
-      <div className="h-[1px] w-full bg-border" />
-      <div className="flex w-full gap-x-2 px-2">
-        <CyberButton className="w-full" variant="secondary">
-            Buy
-        </CyberButton>
+      <div className="relative flex aspect-[16/8] h-auto w-full items-center justify-center overflow-hidden rounded-b">
+        <TemplateStack stack={stack} />
+        <Image
+          src={image}
+          alt={name}
+          placeholder="blur"
+          className="h-full w-full object-cover"
+          fill
+        />
+        <div className="anim absolute -bottom-full left-0 z-10 flex w-full items-center justify-around border-t bg-popover px-3 group-hover:bottom-0">
+          {/* Star */}
+          <div className="flex items-center gap-x-1 py-3">
+            <code className="text-xs leading-none text-accent-foreground">
+              {type}
+            </code>
+          </div>
+          <div className="h-auto w-[1px] self-stretch bg-border" />
+          <div className="flex items-center gap-x-1 py-3">
+            <code className="text-xs leading-none text-accent-foreground underline underline-offset-2">
+              {price}
+            </code>
+          </div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 export default TemplateCard;
